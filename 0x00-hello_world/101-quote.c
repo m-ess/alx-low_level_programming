564e00fb093a560214b4818b327a118bd2dc72e5
mohamed <medess313@gmail.com>
2023-08-11 08:44:42 -0700

    6-size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <wnistd.h>
 /**
  * main - A c program that prints a line to the standard error
- * return: 1 (success)
+ * return 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+        fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
