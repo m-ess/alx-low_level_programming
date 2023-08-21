b15988d47d22968b527ff5e01bb2123c12f8ce96
mohamed <medess313@gmail.com>
2023-08-21 15:01:27 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -9,11 +9,11 @@ void reset_to_98(int *);
  */
 int main(void)
 {
-	int n;
+        int n;
 
-	n = 402;
-	printf("%d\n", n);
-	reset_to_98(&n);
-	printf("%d\n", n);
-	return (0);
+        n = 402;
+        printf("%d\n", n);
+        reset_to_98(&n);
+        printf("%d\n", n);
+        return (0);
 }
