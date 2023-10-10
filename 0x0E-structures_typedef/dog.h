b12fb8cf3133ee80c2706bcdd367dbdf6b954dae
m-ess <medess313@gmail.com>
2023-10-10 14:28:21 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - DOG
+ * @name: input
+ * @age: input
+ * @owner: input
+ * Desc: header file
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+
+#endif
