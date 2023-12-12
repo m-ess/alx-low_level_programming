ef66d8e4315fd79f1e3bba0fd77dc3ab20a43306
m-ess <medess313@gmail.com>
2023-12-12 11:20:06 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+include "lists.h"
 
 /**
  * insert_nodeint_at_index - inserts a new node in a linked list,
@@ -29,3 +29,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+
+	return (NULL);
+}
