26cc24a99524dd21d0b1e57263a8c02c2a4b7d06
m-ess <medess313@gmail.com>
2023-12-22 09:56:14 -0800

    3-cp.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stddef.h>
+/**
+ * read_textfile - reads a text file and prints stdout
+ * @filename: input
+ * @letters: input
+ * Return: Number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fdopen, fdread, fdwrite;
+	char *buf;
+
+	fdopen = open(filename, O_RDWR);
+	buf = malloc(sizeof(char) * letters);
+
+	if (fdopen == -1 || filename == NULL)
+		return (0);
+	if (buf == NULL)
+		return (0);
+
+	fdread = read(fdopen, buf, letters);
+	fdwrite = write(STDOUT_FILENO, buf, fdread);
+	close(fdopen);
+	free(buf);
+
+	return (fdwrite);
+}
