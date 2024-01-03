8305f3797932a99326f5e8efb94ede9d08c7ec68
m-ess <medess313@gmail.com>
2024-01-03 08:54:46 -0800

    simple_shell

---
+++ simple_shell/_atoi.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ * _atoi -  a function that converts a string to integers
+ * @str: input
+ *
+ * Return: the resulting integer
+ */
+int _atoi(const char *str)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	if (str[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (-2);
+
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+
+	return (sign * result);
+}
